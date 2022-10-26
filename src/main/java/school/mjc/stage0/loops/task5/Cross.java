2b541b10fe0414ce178401dc14c683868d5c7014
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-10-26 16:37:41 +0600

    26/10/2022/16:37

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 0; i < sideLength; i++) {
+            if( i+ 1 == sideLength % 2 + sideLength / 2){
+                for (int j = 0; j < sideLength; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+                continue;
+            }
+            for (int j = 0; j < sideLength; j++) {
+                if (sideLength % 2 + sideLength / 2 == j + 1){
+                    System.out.print("8");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,33 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int number = height;
+        int count = -2;
+        for (int i = 0; i < height; i++) {
+            if (number <= 0){
+                count = 2;
+                number+=count;
+                if(number == 1){
+                    number++;
+                }
+                for (int n = 0; n < height; n++) {
+                    if ((height - number) / 2 <= n && height - (height - number) / 2 - 1 >= n){
+                        System.out.print("8");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+            }else {
+                for (int j = 0; j < height; j++) {
+                    if ((height - number) / 2 <= j && height - (height - number) / 2 - 1 >= j){
+                        System.out.print("8");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            number += count;
+            System.out.println();
+        }
     }
 }
